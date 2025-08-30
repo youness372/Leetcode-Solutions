33898f75f0a9e4d43e8b3e79e2847b9933222cc2
younsse347 <younessagrine2@gmail.com>
2025-08-30 10:50:32 +0100

    Create valideSudoku.java

---
+++ Daily Day/05-30 August 2025/valideSudoku.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+        
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+                
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                
+                if (rows[i].contains(c) || cols[j].contains(c) || boxes[boxIndex].contains(c)) {
+                    return false;
+                }
+                
+                rows[i].add(c);
+                cols[j].add(c);
+                boxes[boxIndex].add(c);
+            }
+        }
+        
+        return true;
+    }
+}
+
